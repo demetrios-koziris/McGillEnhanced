aba55c237a742da42e67182692cc93214c9a07fb
Demetrios-Koziris <dwkoziris@gmail.com>
2016-06-15 06:25:46 -0400

    adjusted quicklinks menu style and content

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,5 @@
+//From https://stackoverflow.com/questions/8915845/chrome-extension-open-a-link-from-popup-html-in-a-new-tab
+
 document.addEventListener('DOMContentLoaded', function () {
     var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
