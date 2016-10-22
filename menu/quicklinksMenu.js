3a8b9b31e664f91992c79bcfbd0820ba3fed0c8e
Demetrios Koziris <dwkoziris@gmail.com>
2016-10-22 19:42:30 -0400

    simplified script for opening links in quicklinks menu

--- menu/quicklinksMenu.js
+++ menu/quicklinksMenu.js
@@ -1,14 +1,7 @@
 //From https://stackoverflow.com/questions/8915845/chrome-extension-open-a-link-from-popup-html-in-a-new-tab
 
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
-    }
-});
+window.addEventListener('click',function(e){
+  if(e.target.href!==undefined){
+    chrome.tabs.create({url:e.target.href})
+  }
+})
