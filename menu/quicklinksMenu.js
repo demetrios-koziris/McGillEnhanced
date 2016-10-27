2777116082789304016998a55dfa444893ee6087
Demetrios Koziris <dwkoziris@gmail.com>
2016-10-26 23:55:10 -0400

    improved links in quicklinks menu without js needed and compatible with firefox

--- menu/quicklinksMenu.js
+++
@@ -1,7 +0,0 @@
-//From https://stackoverflow.com/questions/8915845/chrome-extension-open-a-link-from-popup-html-in-a-new-tab
-
-window.addEventListener('click',function(e){
-  if(e.target.href!==undefined){
-    chrome.tabs.create({url:e.target.href})
-  }
-})
