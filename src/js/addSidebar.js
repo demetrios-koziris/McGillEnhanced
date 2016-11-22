72695740309763e3edc6ee505fb4bf54a64c9e83
demetrios-koziris <dwkoziris@gmail.com>
2016-11-22 08:27:49 -0500

    generic getPage function to get html from url

--- src/js/addSidebar.js
+++ src/js/addSidebar.js
@@ -15,8 +15,28 @@ The GNU General Public License can also be found at <http://www.gnu.org/licenses
 //jshint esversion: 6
 
 
+function getPage(url) {
+        const xmlRequestInfo = {
+        method: 'GET',
+        action: 'xhttp',
+        url: url
+    };
+    chrome.runtime.sendMessage(xmlRequestInfo, function(data) {
+        const htmlParser = new DOMParser();
+        const htmlDoc = htmlParser.parseFromString(data.responseXML, "text/html");
+
+        // tbody = htmlDoc.getElementsByClassName('datadisplaytable')[0];
+
+        // mainColumn = document.getElementById('main-column');
+        // mainColumn.appendChild(tbody);
+        // console.log(htmlDoc);
+        // console.log(data.responseXML);
+    });
+}
+
 function makeSidebarContent() {
 
+    getPage("https://horizon.mcgill.ca/pban1/bwskfcls.P_GetCrse_Advanced?rsts=dummy&crn=dummy&term_in=201609&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=COMP&sel_coll=&sel_crse=250&sel_title=&sel_schd=&sel_from_cred=&sel_to_cred=&sel_levl=&sel_ptrm=%25&sel_instr=%25&sel_attr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a&path=1&SUB_BTN=&");
     // fix layout bug on mcgill site for some newStyle pages (2009-2010 & 2011-2012)
     if (isNewStyle) {
         document.getElementById("inner-container").style.width = '100%';
