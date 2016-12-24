45abf8fdf8c2d81704a1ccb2822aa9fe84b96348
Demetrios Koziris <dwkoziris@gmail.com>
2016-12-24 17:43:34 -0500

    show each instructor only once with icons to indicate which terms they teach

--- src/js/addProfLinks.js
+++ src/js/addProfLinks.js
@@ -16,6 +16,12 @@ The GNU General Public License can also be found at <http://www.gnu.org/licenses
 
 
 function makeProfLinks() {
+
+    terms = {
+        'Fall': 'https://www.mcgill.ca/ece/sites/all/modules/mcgill/courses/fall.gif',
+        'Winter': 'https://www.mcgill.ca/ece/sites/all/modules/mcgill/courses/winter.gif',
+        'Summer': 'https://www.mcgill.ca/ece/sites/all/modules/mcgill/courses/summer.gif'
+    };
     profs = {};
     const profsByTerm = {};
     const minervaProfs = getProfData();
@@ -33,8 +39,6 @@ function makeProfLinks() {
 
     if (!profsFullSource.match(/There are no professors/)) {
 
-        const profSection = document.createElement('div');
-
         const profsFullSourceArray = profsFullSource.match(/(Fall|Winter|Summer)/g);
         for (let a = 0; a < profsFullSourceArray.length; a++) {
             if (!(profsFullSourceArray[a] in profsByTerm)) {
@@ -46,65 +50,93 @@ function makeProfLinks() {
 
         for (let termKey in profsByTerm) {
 
-            const termSection = document.createElement('div');
-            termSection.className = 'mcen-termSection';
-            profSection.appendChild(termSection);
-
             const profsTermSource = profsFullSource.split("(" + termKey + ")");
             if (profsTermSource.length > 1) {
                 profsByTerm[termKey] = profsTermSource[0].split(",");
 
-                const termTitle = document.createElement('div');
-                termTitle.innerHTML = 'Instructors (<b>' + termKey + '</b>) :';
-                termSection.appendChild(termTitle);
-
                 for (let p=0; p<profsByTerm[termKey].length; p++) {
-                    const profDiv = document.createElement('div');
-                    profDiv.className = 'mcen-profDiv';
-                    termSection.appendChild(profDiv);
 
-                    let profName = generateProfNameObject(minervaProfs, profsByTerm[termKey][p]);
-                    const profCoursesURL = "https://www.mcgill.ca/study/" + urlYears + "/courses/search" + (isNewStyle ? "?search_api_views_fulltext=" : "/") + profName.fullName;
-                    profs[profName.fullNameKey] = profName;
-
-                    const googleLink = document.createElement('a');
-                    googleLink.href = 'https://www.google.ca/search?q="rate"+"mcgill"+' + profName.firstName + '+' + profName.lastName;
-                    profDiv.appendChild(googleLink);
-
-                    const googleLinkButton = document.createElement('button');
-                    googleLinkButton.className = 'tooltip mcen-profLinkButton mcen-googleLinkButton';
-                    googleLinkButton.title = googleLinkMessage;
-                    googleLinkButton.innerText = 'Google';
-                    googleLink.appendChild(googleLinkButton);
-
-                    const mercuryLink = document.createElement('a');
-                    mercuryLink.href = "https://horizon.mcgill.ca/pban1/bzskmcer.p_display_form?form_mode=ar&inst_tab_in=" + minervaProfs[profName.fullName];
-                    profDiv.appendChild(mercuryLink);
-
-                    const mercuryLinkButton = document.createElement('button');
-                    mercuryLinkButton.className = 'tooltip mcen-profLinkButton mcen-mercuryLinkButton';
-                    mercuryLinkButton.title = mercuryLinkMessage;
-                    mercuryLinkButton.innerText = 'Mercury';
-                    mercuryLink.appendChild(mercuryLinkButton);
-                    
-                    if (!(profName.fullName in minervaProfs)) {
-                        mercuryLinkButton.className = 'tooltipError mcen-profLinkButton mcen-mercuryLinkButton not-active';
-                        mercuryLinkButton.title = mercuryLinkMessageError;
-                        mercuryLink.href = 'https://horizon.mcgill.ca/pban1/bzskmcer.p_display_form=';
+                    let prof = generateProfObject(minervaProfs, profsByTerm[termKey][p], termKey);
+                    if (prof.key in profs) {
+                        profs[prof.key].terms[termKey] = '';
                     }
-
-                    const profLink = document.createElement('a');
-                    profLink.href = profCoursesURL;
-                    profLink.className = 'tooltip mcen-profLink ' + profName.fullNameKey;
-                    profLink.title = profLinkMessage;
-                    profLink.innerText = profName.fullName;
-                    profDiv.appendChild(profLink);
-
+                    else {
+                        profs[prof.key] = prof;
+                    }
                 }
 
                 logForDebug(profsByTerm[termKey]);
                 profsFullSource = profsTermSource[1];
             }
+        }
+
+        const profSection = document.createElement('div');
+
+        const profSectionTitle = document.createElement('div');
+        profSectionTitle.innerHTML = '<b>Instructors:</b>';
+        profSection.appendChild(profSectionTitle);
+
+        for (let profKey in profs) {
+
+            prof = profs[profKey];
+
+            const profDiv = document.createElement('div');
+            profDiv.className = 'mcen-profDiv';
+            profSection.appendChild(profDiv);
+
+            const googleLink = document.createElement('a');
+            googleLink.href = prof.urlGoogle;
+            profDiv.appendChild(googleLink);
+
+            const googleLinkButton = document.createElement('button');
+            googleLinkButton.className = 'tooltip mcen-profLinkButton mcen-googleLinkButton';
+            googleLinkButton.title = googleLinkMessage;
+            googleLinkButton.innerText = 'Google';
+            googleLink.appendChild(googleLinkButton);
+
+            const mercuryLink = document.createElement('a');
+            mercuryLink.href = prof.urlMercury;
+            profDiv.appendChild(mercuryLink);
+
+            const mercuryLinkButton = document.createElement('button');
+            mercuryLinkButton.className = 'tooltip mcen-profLinkButton mcen-mercuryLinkButton';
+            mercuryLinkButton.title = mercuryLinkMessage;
+            mercuryLinkButton.innerText = 'Mercury';
+            mercuryLink.appendChild(mercuryLinkButton);
+
+            if (!(prof.name.full in minervaProfs)) {
+                mercuryLinkButton.className = 'tooltipError mcen-profLinkButton mcen-mercuryLinkButton not-active';
+                mercuryLinkButton.title = mercuryLinkMessageError;
+                mercuryLink.href = 'https://horizon.mcgill.ca/pban1/bzskmcer.p_display_form=';
+            }
+
+            const termSection = document.createElement('div');
+            termSection.className = 'mcen-termSection';
+            profDiv.appendChild(termSection);
+
+            for (let termKey in terms) {
+
+                const termDiv = document.createElement('div');
+                termDiv.className = 'mcen-termDiv';
+                termSection.appendChild(termDiv);
+
+                const termImg = document.createElement('img');
+                termImg.className = 'mcen-termImg';
+                termDiv.appendChild(termImg);
+
+                if (termKey in prof.terms) {
+                    termDiv.title = termKey;
+                    termDiv.className += '';
+                    termImg.src = terms[termKey];
+                } 
+            }
+
+            const profLink = document.createElement('a');
+            profLink.href = prof.urlCourses;
+            profLink.className = 'tooltip mcen-profLink ' + prof.key;
+            profLink.title = profLinkMessage;
+            profLink.innerText = prof.name.full;
+            profDiv.appendChild(profLink);
         }
 
         document.getElementsByClassName("catalog-instructors")[0].innerHTML = "";
@@ -146,37 +178,30 @@ function makeProfLinks() {
             boxShadow: '2px 2px 10px #E54944'
         }
         });
+
+
     }
 }
 
 
-function encodeSymbolsWin1252(string) {
-    return encodeURI(string).replace(/\'/g, '%27')
-                            .replace(/\-/g, '%20')
-                            .replace(/\%C3\%8/g, '%C')
-                            .replace(/\%C3\%9/g, '%D')
-                            .replace(/\%C3\%A/g, '%E')
-                            .replace(/\%C3\%B/g, '%F');
-}
-
-
-function generateProfNameObject(minervaProfs, origName) {
+function generateProfObject(minervaProfs, origName, term) {
     const name = origName.trim();
     const splitName = name.split(' ');
     const profName = {
-        fullName: name,
-        fullNameKey: name.replace(/\W/g, ''),
-        firstName: splitName[0],
-        lastName: splitName[splitName.length-1],
-        minerva: minervaProfs[name]
+        full: name,
+        first: splitName[0],
+        last: splitName[splitName.length-1]
     };
-    const forURL = {
-        fullName: profName.fullName.replace(/ /g, '&nbsp').replace(/-/g, '&#8209'),
-        firstName: encodeSymbolsWin1252(profName.firstName),
-        lastName: encodeSymbolsWin1252(profName.lastName)
+    const prof = {
+        key: name.replace(/\W/g, ''),
+        terms: {},
+        minerva: minervaProfs[name],
+        name: profName,
+        urlCourses: 'https://www.mcgill.ca/study/' + urlYears + '/courses/search' + (isNewStyle ? '?search_api_views_fulltext=' : '/') + profName.full,
+        urlGoogle: 'https://www.google.ca/search?q="rate"+"mcgill"+' + profName.first + '+' + profName.last,
+        urlMercury: 'https://horizon.mcgill.ca/pban1/bzskmcer.p_display_form?form_mode=ar&inst_tab_in=' + minervaProfs[profName.full]
     };
-    profName.forURL = forURL;
-
-    return profName;
+    prof.terms[term] = '';
+    return prof;
 }
 
