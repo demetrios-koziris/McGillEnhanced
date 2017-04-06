3114a84403dc27c98a6d7aaea74360bac2276d7d
Demetrios Koziris <dwkoziris@gmail.com>
2017-04-05 21:28:04 -0400

    add mcgill discord to 'Other Resources' section of extension button menu

--- src/menu/quicklinksMenu.js
+++ src/menu/quicklinksMenu.js
@@ -1,3 +1,19 @@
+/*
+McGill Enhanced is a chrome extension that improves the functionality and navigability of McGill.ca
+Copyright (C) 2016 Demetrios Koziris
 
-let isDevVersion = !('update_url' in chrome.runtime.getManifest())
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+A copy of the GNU General Public License is provided in the LICENSE.txt file along with this program.  
+The GNU General Public License can also be found at <http://www.gnu.org/licenses/>.
+*/
+
+//jshint esversion: 6
+
+
+let isDevVersion = !('update_url' in chrome.runtime.getManifest());
 document.getElementById('version').innerText = 'Version ' + chrome.app.getDetails().version + (isDevVersion ? ' DEV' : '');
